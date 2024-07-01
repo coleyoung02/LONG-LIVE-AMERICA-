d5012494dbde016db3fdba1752fea8b6402eda0b
Funbam <bambamtheram@gmail.com>
2024-06-30 18:28:04 -0700

    doing stuff

--- Assets/Scripts/CharacterManager.cs
+++ Assets/Scripts/CharacterManager.cs
@@ -10,9 +10,13 @@ public class CharacterManager : MonoBehaviour
     [SerializeField] float scaleChange;
     [SerializeField] Transform spawnPoint;
     [SerializeField] float positionVariance;
+    [SerializeField] float shrinkConstant;
+    [SerializeField] float maxScale;
+    [SerializeField] float minScale;
+    private QuestionManager qm;
     protected virtual void Start()
     {
-
+        qm = FindAnyObjectByType<QuestionManager>();
     }
 
     public void OnSuccess(string content)
@@ -28,7 +32,23 @@ public class CharacterManager : MonoBehaviour
     {
         head.localScale = head.localScale / scaleChange;
         GameObject newbubble = Instantiate(failurePrefab, spawnPoint.position, Quaternion.identity);
-        Vector3 changeVector = new Vector3(Random.Range(0, positionVariance), Random.Range(0, positionVariance), 0);
+        Vector3 changeVector = new Vector3(Random.Range(-positionVariance, positionVariance), Random.Range(-positionVariance, positionVariance), 0);
         newbubble.transform.position += changeVector;
+    }
+
+    private void Update()
+    {
+        if (qm.PlayerCanRespond())
+        {
+            head.transform.localScale -= new Vector3(shrinkConstant * Time.deltaTime, shrinkConstant * Time.deltaTime, 0);
+        }
+        if(head.transform.localScale.x < minScale)
+        {
+            head.transform.localScale = new Vector3(minScale, minScale, head.transform.localScale.z);
+        }
+        if (head.transform.localScale.x > maxScale)
+        {
+            head.transform.localScale = new Vector3(maxScale, maxScale, head.transform.localScale.z);
+        }
     }
 }
